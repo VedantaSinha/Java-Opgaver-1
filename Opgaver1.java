805d26ed5ce274e2cf48497c6d6b139179b7973e
VedantaSinha <sinha.vedanta@gmail.com>
2026-01-08 10:15:13 +0100

    Update Opgaver1.java

--- Opgaver1.java
+++ Opgaver1.java
@@ -1,6 +1,26 @@
-public class Opgaver1 {
+import java.util.Scanner;
+
+class RightTriangle {
+
     public static void main(String[] args) {
-        System.out.println("This is Opgaver 1");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+        double a = input.nextDouble();
+
+        System.out.print("Enter b: ");
+        double b = input.nextDouble();
+
+        if (a < 0 || b < 0) {
+            System.out.println("Illegal value: a and b must be non-negative.");
+            return;
+        }
+
+        double c = Math.sqrt(a * a + b * b);
+        double area = 0.5 * a * b;
+
+        System.out.println("Hypotenuse c = " + c);
+        System.out.println("Area = " + area);
     }
 }
-
