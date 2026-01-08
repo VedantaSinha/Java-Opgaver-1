64e2af4e1de7e8ec5b58f676ca2fcf21ccc89c48
VedantaSinha <sinha.vedanta@gmail.com>
2026-01-08 10:27:27 +0100

    Update Opgaver4Client.java

--- Opgaver4Client.java
+++ Opgaver4Client.java
@@ -1,6 +1,44 @@
-public class Opgaver4 {
-    public static void main(String[] args) {
-        System.out.println("This is Opgaver 4");
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Opgaver4Client {
+
+    static final String HOST = "127.0.0.1";
+    static final int PORT = 1068;
+
+    static Scanner input;
+
+    public static void main(String[] args) throws Exception {
+        setup();
+        run();
+        close();
+    }
+
+    static void setup() {
+        input = new Scanner(System.in);
+    }
+
+    static void run() throws Exception {
+        System.out.print("Enter text: ");
+        String text = input.nextLine();
+
+        Socket sock = new Socket(HOST, PORT);
+
+        BufferedReader netIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        PrintWriter netOut = new PrintWriter(sock.getOutputStream(), true);
+
+        netOut.println(text);
+
+        String reply = netIn.readLine();
+        System.out.println("Reversed: " + reply);
+
+        sock.close();
+    }
+
+    static void close() {
+        input.close();
     }
 }
-
